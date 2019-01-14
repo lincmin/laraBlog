40cd2a2f8a0b2deb6b8bcdfe8f8f58a395479b1b
lincmin <lincm69696@qq.com>
2019-01-14 23:21:52 +0800

    add Field

--- database/migrations/2019_01_14_141905_create_posts_table.php
+++ database/migrations/2019_01_14_141905_create_posts_table.php
@@ -15,6 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('content');
+            $table->softDeletes();
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
