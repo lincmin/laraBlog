85592b11d8de1bf26cac886bf095361a71beadba
lincmin <lincm69696@qq.com>
2019-01-30 22:21:22 +0800

    UploadFileRequest

--- app/Http/Requests/UploadFileRequest.php
+++ app/Http/Requests/UploadFileRequest.php
@@ -13,7 +13,7 @@ class UploadFileRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class UploadFileRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'file' => 'required',
+            'folder' => 'required',
         ];
     }
 }
